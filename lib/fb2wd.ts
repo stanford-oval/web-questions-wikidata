d7f911de50078cd59cac887a35ba99a0055c713a
Silei Xu <ray.slxu@gmail.com>
2022-06-28 15:25:49 -0700

    Include util functions from qald and fix syntax issues in WebQuestions

--- lib/fb2wd.ts
+++ lib/fb2wd.ts
@@ -1,6 +1,36 @@
 import fs from 'fs';
 import path from 'path';
 import * as argparse from 'argparse';
+import { 
+    Parser, 
+    SparqlParser, 
+    Generator,
+    SparqlGenerator,
+    SelectQuery, 
+    AskQuery, 
+    Pattern
+} from 'sparqljs';
+
+/**
+ * WebQuestion SPARQL misses xsd prefix
+ *  - PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+ * WebQuestion SPARQL also comes with some fixed filters for all queries that are unnecessary for parsing
+ *  - a filter to make sure the result is not a mentioned entity in the question
+ *  - a filter to make sure the result is either (1) an entity (2) language is not specified or English
+ * @param sparql the original sparql in web questions dataset
+ * @returns a preprocessed sparql that parses
+ */
+function preprocessWebQuestionsSparql(sparql : string) {
+    sparql = 'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n' + sparql;
+    sparql = sparql.replace(/\\n/g, '\n');
+    sparql = sparql.replace(/FILTER \(\?x \!= ns:m.[^)]+\)/g, '')
+    sparql = sparql.replace(`FILTER (!isLiteral(?x) OR lang(?x) = '' OR langMatches(lang(?x), 'en'))`, '');
+    sparql = sparql.replace(`FILTER (!isLiteral(?x) OR (lang(?x) = '' OR lang(?x) = 'en'))`, '');
+    sparql = sparql.replace(/[\n]+/g, '\n');
+    sparql = sparql.replace(/ OR /g, '||');
+    sparql = sparql.replace(`Having COUNT(?city) = 2`, `Having (COUNT(?city) = 2)`);
+    return sparql;
+}
 
 interface WebQuestionExample {
     QuestionId : string,
@@ -14,10 +44,14 @@ interface WebQuestionParse {
 }
 
 class FB2WDConverter {
+    private parser : SparqlParser;
+    private generator : SparqlGenerator;
     private entityMappings : Record<string, string>;
     private propertyMappings : Record<string, string>;
 
     constructor() {
+        this.parser = new Parser();
+        this.generator = new Generator();
         this.entityMappings = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/entity-mappings.json'), 'utf-8'));
         this.propertyMappings = JSON.parse(fs.readFileSync(path.join(__dirname, `../../data/property-mappings.json`), 'utf-8'));
     }
@@ -30,8 +64,23 @@ class FB2WDConverter {
         return this.propertyMappings[fb_id];
     }
 
+    private _convertWhereClause(clause : Pattern) {
+
+    }
+
     convert(sparql : string) {
-        return sparql;
+        const preprocessedSparql = preprocessWebQuestionsSparql(sparql);
+        try {
+            const parsed = this.parser.parse(preprocessedSparql) as SelectQuery|AskQuery;
+            if (parsed.where) {
+                for (const clause of parsed.where)
+                    this._convertWhereClause(clause);
+            }
+            return this.generator.stringify(parsed);
+        } catch(e) {
+            console.log(preprocessedSparql);
+            console.log((e as Error).message);
+        }
     }
 }
 
@@ -57,9 +106,7 @@ async function main() {
             sparql: converter.convert(ex.Parses[0].Sparql)
         }
     });
-    fs.writeFileSync(args.output, JSON.stringify(examples, null, 2));
-
-    
+    fs.writeFileSync(args.output, JSON.stringify(examples, null, 2));    
 }
 
 if (require.main === module)
---
+++ lib/utils/sparqljs-typeguard.ts
@@ -0,0 +1,65 @@
+import { ENTITY_PREFIX, PROPERTY_PREFIX } from './wikidata';
+import { 
+    IriTerm,
+    VariableTerm,
+    LiteralTerm,
+    PropertyPath,
+    BgpPattern,
+    FilterPattern,
+    UnionPattern,
+    AggregateExpression
+} from 'sparqljs';
+
+export function isNamedNode(node : any) : node is IriTerm {
+    return 'termType' in node && node.termType === 'NamedNode';
+}
+
+export function isWikidataEntityNode(node : any) : node is IriTerm {
+    return 'termType' in node && node.termType === 'NamedNode' && node.value.startsWith(ENTITY_PREFIX);
+}
+
+export function isWikidataPropertyNode(node : any, pid ?: string) : node is IriTerm {
+    if (pid)
+        return 'termType' in node && node.termType === 'NamedNode' && node.value === PROPERTY_PREFIX + pid;
+    return 'termType' in node && node.termType === 'NamedNode' && node.value.startsWith(PROPERTY_PREFIX);
+}
+
+export function isVariable(node : any) : node is VariableTerm {
+    return 'termType' in node && node.termType === 'Variable';
+}
+
+export function isLiteral(node : any) : node is LiteralTerm {
+    return 'termType' in node && node.termType === 'Literal';
+}
+
+export function isPropertyPath(node : any) : node is PropertyPath {
+    return 'pathType' in node && ['|', '/', '^', '+', '*', '!'].includes(node.pathType);
+}
+
+export function isSequencePropertyPath(node : any) : node is PropertyPath {
+    return 'pathType' in node && node.pathType === '/' && node.items.length > 1;
+}
+
+export function isUnaryPropertyPath(node : any, type ?: '+'|'*'|'!') : node is PropertyPath {
+    if (type)
+        return 'pathType' in node && node.pathType === type && node.items.length === 1;
+    return 'pathType' in node && ['+', '*', '!'].includes(node.pathType) && node.items.length === 1;
+}
+
+export function isBasicGraphPattern(node : any) : node is BgpPattern {
+    return 'type' in node && node.type === 'bgp';
+}
+
+export function isFilterPattern(node : any) : node is FilterPattern {
+    return 'type' in node && node.type === 'filter';
+}
+
+export function isUnionPattern(node : any) : node is UnionPattern {
+    return 'type' in node && node.type === 'union';
+}
+
+export function isAggregateExpression(node : any, aggregation ?: string) : node is AggregateExpression {
+    if (aggregation)
+        return 'type' in node && node.type === 'aggregate' && node.aggregation === aggregation;
+    return 'type' in node && node.type === 'aggregate';
+}
